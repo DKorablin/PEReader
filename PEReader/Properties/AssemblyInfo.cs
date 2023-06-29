f67030816e0cde4e82a5f6c1ec02f465cde0116c
Danila Korablin <AlphaOmega@narod.ru>
2023-06-29 19:53:41 +0300

    Pushing assembly description attributes from AssemblyInfo.cs to .csproj file (To sync assembly version and git version)

--- PEReader/Properties/AssemblyInfo.cs
+++ PEReader/Properties/AssemblyInfo.cs
@@ -1,23 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("PE Reader")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Portable Executable Reader")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2012-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("ee008adf-5990-4e3e-83a3-b8f1cebc4957")]
 
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: System.CLSCompliant(false)]
